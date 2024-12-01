b1e7797384ea2565bac0d98b922d4926e23a7bb6
ailuroken <aiyuyao2000@gmail.com>
2024-12-01 20:20:13 +0000

    completed part 2 of day one and added images

--- day-one/day_one.py
+++ day-one/day_one.py
@@ -11,4 +11,21 @@ def part_one():
 
     return answer
 
+def part_two():
+    answer = 0
+    right_dict = dict()
+
+    for elem in right_list:
+        if elem in right_dict:
+            right_dict[elem] += 1
+        else:
+            right_dict[elem] = 1
+
+    for elem in left_list:
+        if elem in right_dict:
+            answer += elem * right_dict[elem]
+
+    return answer
+
 print(part_one())
+print(part_two())
